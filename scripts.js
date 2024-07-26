649df7745cb3590abb20dbcc015930c4995ef8ba
Tlalemasego <tlalemasego@gmail.com>
2024-07-26 13:51:54 +0200

    git commit - completed the log products, filter by name length, price manipulation, concatenation, object transformation and extremes in prices task

--- scripts.js
+++ scripts.js
@@ -14,6 +14,9 @@ const products = [
   { product: 'tea', price: '' },
 ]
 
+
+// EXERCISES
+
 // Logging each name in the console 
 names.forEach(name => console.log(name));
 
@@ -33,4 +36,68 @@ console.log(upperCaseProvinces)
 const nameLength = names.map(name => name.length)
 console.log(nameLength);
 
-//
+// Sorting the provinces in alphatecal order 
+const alphabeticalOrder = provinces.sort();
+console.log(alphabeticalOrder);
+
+// Mapping through the names array for names with 'S'
+const findingS = names.map(name => [...name].some(char => char === 'S'));
+console.log(findingS);
+
+// Transforming names array into an object mapping names to respective provinces. 
+ const objectMapping = names.reduce((accumulator, name, province) => {  
+  accumulator[name] =provinces[province];
+  return accumulator;
+ }, {});
+
+ console.log(objectMapping);
+
+ //ADVANCED EXCERCISES 
+
+ // Iterating throught the products array to log each product name
+ products.forEach(item => console.log(item.product));
+
+ // Filtering out products with names longer than 5 characters 
+ products.filter(item => item.product.length > 5) // The function checks if the length of the product name is greater than 5
+ .forEach(item => console.log(item.product));
+
+ // Filterng out products without prices, converting those with string numbers and calculating the total 
+ const priceManipulation = products.filter(item => String(item.price).trim() !== '') // Filters products with valid prices
+  .map(item => Number(item.price)) // Converts string prices to numbers 
+  .reduce((accumulator, price) => accumulator + price); // calculates total price 
+  console.log(priceManipulation)
+
+
+  // Concatenated product names 
+  const concatenatedProducts = products.reduce((accumulator, item) => 
+    accumulator + item.product);
+ 
+  console.log(concatenatedProducts)
+
+  // Filtering Highest and lowest prices 
+  const prices = products.filter(item => String(item.price).trim() !== '') // Filters products with valid prices
+  .map(item => Number(item.price)) // Converts string prices to numbers
+ 
+    const highestPrice = Math.max(...prices); // Finds the highest price item
+    const lowestPrice = Math.min(...prices // Finds the lowest priced item
+    );
+    const result = `Highest: ${highestPrice}. Lowest: ${lowestPrice}.`;
+    console.log(result)
+
+  // Recreating the products object with keys 'name' and 'cost' 
+  const keyChanges = products.map(productsObject => // Iterates over each product object
+    Object.entries(productsObject).reduce((accumulator, [key, value]) => {  // Object entries converts each product object into an array of key, value pairs.  
+      if (key === 'product') {  // Sets the name property 
+        accumulator.name = value;
+      } else if (key === 'price') {  // Converts the price to a number and sets the costs property 
+        accumulator.cost = Number(value); 
+      }
+      return accumulator;
+      }, {})
+    );
+
+    console.log(keyChanges)
+
+
+
+
